613cc9a5ee0a01ac924a6763b69f4034a0939339
sonnguyenan <nguyenanson20001100@gmail.com>
2023-08-16 17:05:12 +0700

    update

--- src/page/Login/login.js
+++ src/page/Login/login.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { FaUserAlt } from "react-icons/fa";
 import { BiSolidLockAlt } from "react-icons/bi";
-import './login.css'
+import './login.css';
+import { Link } from "react-router-dom"
 
 const Login = () => {
+
+  const divStyle = {
+    padding: '0 40px',
+  };
+
   return (
     <div className='LoginForm'>
       <div className='container'>
-        <div className='form-box Login'>
+        <div className='form-box Login' style={divStyle}>
           <h2>Login</h2>
           <form action='#'>
             <div className='input-box'>
               <input type='text' required></input>
               <label for=""> Username</label>
-              <FaUserAlt />
+              <FaUserAlt className='icon' />
             </div>
 
             <div className='input-box'>
               <input type='text' required></input>
               <label for=""> Password</label>
-              <BiSolidLockAlt />
+              <BiSolidLockAlt className='icon' />
             </div>
 
             <div className='input-box'>
-              <button className='login-btn' type='submit'> Login </button>
+              <button className='btn' type='submit'> Login </button>
             </div>
 
             <div className='regi-link'>
-              <p>Don't have an account ? 
-                <a href='#' className='Login_signin'>Sign in</a>  
+              <p>Don't have an account ?
+                <Link to={"/signup"}>Sign up </Link>
               </p>
 
             </div>
--- src/page/Signup/signup.js
+++ src/page/Signup/signup.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { FaUserAlt } from "react-icons/fa";
+import { BiSolidLockAlt } from "react-icons/bi";
+import './signup.css';
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+
+  const divStyle = {
+    padding: '0 40px',
+  };
+
   return (
-    <div>signup</div>
+    <div className='LoginForm'>
+      <div className='container'>
+        <div className='form-box Login' style={divStyle}>
+          <h2>Login</h2>
+          <form action='#'>
+            <div className='input-box'>
+              <input type='text' required></input>
+              <label for=""> First Name</label>
+              <FaUserAlt className='icon'/>
+            </div>
+
+            <div className='input-box'>
+              <input type='text' required></input>
+              <label for=""> Last Name</label>
+              <FaUserAlt className='icon'/>
+            </div>
+
+            <div className='input-box'>
+              <input type='email' required></input>
+              <label for=""> Email </label>
+              <FaUserAlt className='icon'/>
+            </div>
+
+            
+
+            <div className='input-box'>
+              <input type='password' required></input>
+              <label for=""> Password</label>
+              <BiSolidLockAlt className='icon' />
+            </div>
+
+            <div className='input-box'>
+              <input type='password' required></input>
+              <label for=""> Confirm Password</label>
+              <BiSolidLockAlt className='icon' />
+            </div>
+
+            <div className='input-box'>
+              <button className='btn' type='submit'> Sign Up </button>
+            </div>
+
+            <div className='regi-link'>
+              <p>Don't have an account ? 
+                <Link to={"login"}> Sign in </Link>
+              </p>
+
+            </div>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
